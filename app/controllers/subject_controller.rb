b1f0556a637f0083a64811ae093cb7ced8443d19
Alyssa Pratt <snubly1@gmail.com>
2016-08-18 10:55:58 -0400

    validation check for interests

--- app/controllers/subject_controller.rb
+++ app/controllers/subject_controller.rb
@@ -2,10 +2,15 @@ class SubjectController < ApplicationController
   def index
     @subject = Subject.all.includes(:posts => [:user])
     @interests_array = []
-    @interests = JSON.parse(current_user.interests).each do |f|
-      if Subject.find(f)
-        @interests_array.push(Subject.find(f))
+    if current_user.interests
+      @interests = JSON.parse(current_user.interests).each do |f|
+        if Subject.find(f)
+          @interests_array.push(Subject.find(f))
+        end
       end
+    else
+      flash[:danger] = "you do not have any interests selected. Please select some in Edit Profile"
+      redirect_to :back
     end
   end
 end
